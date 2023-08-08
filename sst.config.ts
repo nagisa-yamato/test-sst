a79ca3e29a9684c1854e73cd7ca5b0d58dc4e13e
Shogo Yamato <yamato@nagisa-inc.jp>
2023-08-08 13:51:50 +0900

    feat: use aws sso

--- sst.config.ts
+++ sst.config.ts
@@ -2,19 +2,21 @@ import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
-  config(_input) {
+  config(input) {
     return {
       name: "test-sst",
       region: "ap-northeast-1",
+      profile: `nagisa-yamato-${input.stage === "production" ? "prod" : "dev"}`,
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site");
-
       stack.addOutputs({
         SiteUrl: site.url,
       });
     });
+    // https://docs.sst.dev/advanced/removal-policy#changing-the-removal-policy
+    app.setDefaultRemovalPolicy("destroy");
   },
 } satisfies SSTConfig;
